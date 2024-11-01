ede2a7b79bb579406df59f2a49ae3050587acb4e
Therese Perswalld <therese.perswalld@chasacademy.se>
2024-11-01 23:34:54 +0100

    JS

--- js/start.js
+++ js/start.js
@@ -1,2 +1,28 @@
-const title=document.createElement("h1");
-title.textContent ="Quiz"; 
+'use strict';
+
+const startpage = document.getElementById("startpage");
+
+function createStartPage(){
+
+    //Head container for the startpage
+    const startPage = document.createElement('startpage');
+    startPage.id = 'startpage';
+
+    //Container for the quiz-titel
+    const container=dokcument.createElement('container');
+    container.id='container';
+    container.className ='container';
+
+    const title=document.createElement('h1');
+    title.textContent ='Quiz'; 
+    container.appendChild(title);
+
+    
+
+
+
+}
+
+
+
+
