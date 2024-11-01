a6989f89c9c45471379268648f0ae01ea8905de7
Therese Perswalld <therese.perswalld@chasacademy.se>
2024-11-02 00:17:08 +0100

    changejs

--- js/start.js
+++ js/start.js
@@ -17,12 +17,24 @@ function createStartPage(){
     title.textContent ='Quiz'; 
     container.appendChild(title);
 
+    function createButtonContainer() {
+        // Create button-container 
+        const buttonContainer = document.createElement('buttoncontainer');
+        buttonContainer.id = 'button-container';
+        buttonContainer.className = 'button-container';
     
-
-
-
+        // List with buttontext
+        const buttonTexts = ['Djur', 'Sport', 'Musik'];
+    
+        // Create buttons based on the list
+        buttonTexts.forEach(text => {
+            const button = document.createElement('button');
+            button.id = 'button-container__button';
+            button.className = 'button-container__button';
+            button.textContent = text; 
+            buttonContainer.appendChild(button); // Add the button to the button container
+        });
+    
+        document.body.appendChild(buttonContainer);
+    }
 }
-
-
-
-
