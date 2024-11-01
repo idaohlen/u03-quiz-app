d69ed3e378cc5f72cca0aa0f6f0b5cec53d20db5
Martinsodersten <martin_sodersten@hotmail.com>
2024-11-01 11:29:02 +0100

    Nested elements

--- end.js
+++ end.js
@@ -6,20 +6,35 @@ function displayEndPage () {
 
     const header = document.createElement('header');
     header.className = 'result-header';
+    main.appendChild(header);
 
     const h1 = document.createElement('h1');
     h1.textContent = 'Slutresultat';
+    header.appendChild(h1);
 
     const paragraph = document.createElement('p');
     paragraph.textContent = '10 av 10 rätt!';
-    paragraph.id = 'score';
+    paragraph.id = 'showScore';
+    paragraph.className = 'show-score';
+    header.appendChild(paragraph);
+
+    const resultBtn = document.createElement('button');
+    resultBtn.textContent = 'Visa resultat';
+    resultBtn.id = 'resultBtn';
+    resultBtn.className = 'result-btn'
+    header.appendChild(resultBtn);
+
+
+
+
+
+
+
+
+
+
 
     
-
-
-
-
-
     
 }
 displayEndPage()
