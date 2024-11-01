f415bbe4b50aa0e0b9465bdda810ada4bb52f21f
Martinsodersten <martin_sodersten@hotmail.com>
2024-11-01 11:05:07 +0100

    Main Header P

--- end.js
+++ end.js
@@ -1,1 +1,25 @@
 'use strict';
+
+function displayEndPage () {
+    const main = document.createElement('main');
+    main.className = 'result-main';
+
+    const header = document.createElement('header');
+    header.className = 'result-header';
+
+    const h1 = document.createElement('h1');
+    h1.textContent = 'Slutresultat';
+
+    const paragraph = document.createElement('p');
+    paragraph.textContent = '10 av 10 rätt!';
+    paragraph.id = 'score';
+
+    
+
+
+
+
+
+    
+}
+displayEndPage()
